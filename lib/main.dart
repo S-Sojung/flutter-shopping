7064e40b4f031faedf474f6ecd9c8331fa928f11
S-Sojung <80329856+S-Sojung@users.noreply.github.com>
2023-03-27 17:41:13 +0900

    장바구니앱 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping/components/shoppingcart_detail.dart';
+import 'package:flutter_shopping/components/shoppingcart_header.dart';
+import 'package:flutter_shopping/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +12,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme(),
+      home: ShoppingCartPage(),
+    );
   }
 }
+
+class ShoppingCartPage extends StatelessWidget {
+  const ShoppingCartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildShoppingCartAppBar(),
+      body: Column(
+        children: [
+          ShoppingcartHeader(),
+          ShoppingcartDetail()
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildShoppingCartAppBar() {
+    return AppBar(
+      leading: Icon(Icons.arrow_back_ios),
+      actions: [
+        IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.shopping_cart_sharp)
+        ),
+        SizedBox(width: 16,)
+      ],
+      elevation: 0.0,
+    );
+  }
+}
+
