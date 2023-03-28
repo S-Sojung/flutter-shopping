bdfa76791012eff50af7f228e6b38dcf86711668
S-Sojung <80329856+S-Sojung@users.noreply.github.com>
2023-03-28 12:03:49 +0900

    수정

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,26 @@
 
 import 'package:flutter/material.dart';
 
+//상수들을 모아두는 폴더
+// PrimarySwatch (??)
+// 색상에는 k 를 붙여줌
+// 사이즈에는 언더스코어를 _
+const MaterialColor kPrimaryWhite = MaterialColor(
+    0xFFFFFFFF,
+    <int, Color>{
+      100 : Color(0xFFFFFFFF),
+      200 : Color(0xFFFFFFFF),
+      300 : Color(0xFFFFFFFF),
+      400 : Color(0xFFFFFFFF),
+      500 : Color(0xFFFFFFFF),
+      600 : Color(0xFFFFFFFF),
+      700 : Color(0xFFFFFFFF),
+      800 : Color(0xFFFFFFFF),
+      900 : Color(0xFFFFFFFF),
+    }
+);
+
 const kPrimaryColor = Color(0xFFeeeeee);
-const kSecondaryColor = Color(0xFFc6c6c6);
-const kAccentColor = Color(0xFFff7643);
+const kSecondaryColor = Color(0xFFc6c6c6); //기본 색상
+const kAccentColor = Color(0xFFff7643); //활성화 색상
+
--- lib/theme.dart
+++ lib/theme.dart
@@ -4,11 +4,13 @@ import 'package:flutter_shopping/constants.dart';
 
 ThemeData theme(){
   return ThemeData(
+
     appBarTheme: AppBarTheme(
       backgroundColor: kPrimaryColor,
       foregroundColor: Colors.black
     ),
     primaryColor: kPrimaryColor,
+    // primarySwatch: kPrimaryWhite,
     scaffoldBackgroundColor: kPrimaryColor
   );
 }
